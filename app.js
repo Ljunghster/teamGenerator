158aecee4490aa333fe7f1c464d96c51540b2056
Ljunghster <mpljungh@gmail.com>
2020-05-21 21:52:21 -0700

    work in progress

--- app.js
+++ app.js
@@ -1,49 +1,30 @@
-// const { prompt } = require('inquirer')
-// const { writeFile, readFile } = require('fs')
-const express = require('express')
-const { join } = require('path')
-const app = express()
+const { prompt } = require('inquirer')
+const { writeFile, readFile } = require('fs')
+// express is used to write the code for the routes
 // const { promisify } = require('until')
 // const axios = require('axios')
+// axios calls/ requests to routes
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'))
-})
 
-app.get('/engineer', (req, res) => {
-    res.sendFile(join(__dirname, 'engineer.html'))
-})
 
-app.get('/intern', (req, res) => {
-    res.sendFile(join(__dirname, 'intern.html'))
-})
-
-app.get('/manager', (req, res) => {
-    res.sendFile(join(__dirname, 'manager.html'))
-})
-
-app.listen(3000, () => {
-    console.log('http://localhost:3000')
-})
-
-// inquirer.prompt([
-//   {
-//     type: 'input',
-//     name: 'name',
-//     message: 'What is your name?'
-//   },
-//   {
-//     type: 'input',
-//     name: 'ID',
-//     message: 'What is your identification password?'
-//   },
-//   {
-//     type: 'input',
-//     name: 'job',
-//     message: 'What is your position?'
-//   }
-// ])
-//   .then(data => {
-//     console.log(data)
-//   })
-//   .catch(err => console.log(err))
+prompt ([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  },
+  {
+    type: 'input',
+    name: 'ID',
+    message: 'What is your identification password?'
+  },
+  {
+    type: 'input',
+    name: 'job',
+    message: 'What is your position?'
+  }
+])
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => console.log(err))
